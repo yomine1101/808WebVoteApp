22945e34b686919667eb45a083b3c1bd89413d17
yomine1101 <au3au3520@gmail.com>
2024-08-16 01:22:29 +0800

    Revert "first"
    This reverts commit 2f3d186671049ea47203fc47d7f9cf8810edece3.

--- backend/webvoteapp/src/main/java/com/example/webvoteapp/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.webvoteapp.repository;
-
-import com.example.webvoteapp.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-    User findByAccount(String account);
-}
