79b2550772e46b9bc18dcd6c0fdcefe727285c38
yomine1101 <au3au3520@gmail.com>
2024-08-16 04:39:48 +0800

    backend update

---
+++ backend/webvoteapp/src/main/java/com/example/webvoteapp/WebvoteappApplication.java
@@ -0,0 +1,13 @@
+package com.example.webvoteapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebvoteappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebvoteappApplication.class, args);
+	}
+
+}
---
+++ backend/webvoteapp/src/main/java/com/example/webvoteapp/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.example.webvoteapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .anyRequest().permitAll()
+                )
+                .csrf(csrf -> csrf.disable())
+                .headers(headers ->
+                        headers.contentSecurityPolicy(csp ->
+                                csp.policyDirectives("default-src 'self'; script-src 'self' https://trusted.cdn.com;"))
+                );
+        return http.build();
+    }
+
+
+}
